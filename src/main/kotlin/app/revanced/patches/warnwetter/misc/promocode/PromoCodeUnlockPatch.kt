c4835ef320f140cb3622b2eaee360376c23f2f50
Chr0matic1 <101169615+Chr0matic1@users.noreply.github.com>
2024-10-01 23:12:35 +0200

    Update PromoCodeUnlockPatch.kt

--- src/main/kotlin/app/revanced/patches/warnwetter/misc/promocode/PromoCodeUnlockPatch.kt
+++ src/main/kotlin/app/revanced/patches/warnwetter/misc/promocode/PromoCodeUnlockPatch.kt
@@ -5,13 +5,11 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.warnwetter.misc.firebasegetcert.FirebaseGetCertPatch
 import app.revanced.patches.warnwetter.misc.promocode.fingerprints.PromoCodeUnlockFingerprint
 
 @Patch(
     name = "Promo code unlock",
     description = "Disables the validation of promo code. Any code will work to unlock all features.",
-    dependencies = [FirebaseGetCertPatch::class],
     compatiblePackages = [CompatiblePackage("de.dwd.warnapp", ["4.2.2"])]
 )
 @Suppress("unused")
